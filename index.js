9ee25484b92d029b366093fa4d48ccc5f347e61d
Valerie Wood <valeriewd1@gmail.com>
2018-01-11 03:49:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+var a, b;
+
+function add(a, b) {
+ a += b;
+}
+
+function subtract(a, b) {
+  a -= b;
+}
+
+function multiply(a, b) {
+  a *= b;
+}
+function  divide(a, b) {
+  a /= b;
+}
+
+  function inc(n, base) {
+    var parsed = parseInt(n, base);
+    if (isNaN(parsed)) { return n }
+  return parsed n++;
+  }
+  
+  function roughScale(x, base) {
+  var parsed = parseInt(x, base);
+  if (isNaN(parsed)) { return 0 }
+  return parsed * 100;
+}
+
+  dec(n);
+  return n--;
+ 
+  makeInt(n); 
+  parseInt('n');
+
+preserveDecimal(n);
+parseFloat('n');
+return preserveDecimal;
+     
+  preserveDecimal(n) 
+  return NaN;
