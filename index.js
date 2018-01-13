97c7342c59e71e3fde8ebba34cee1dbda07ac63c
Karen O <k.ortiz026@gmail.com>
2018-01-13 04:46:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a,b){
+  return a + b
+}
+
+function subtract(c,d){
+  return c - d
+}
+
+function multiply(e,f){
+  return e * f
+}
+
+function divide(g,h){
+  return g/h
+}
+
+function inc(n){
+  return n += 1
+}
+
+function dec(n){
+  return n -= 1
+}
+
+function makeInt(A){
+  return parseInt(`${A}`, 10)
+}
+
+function preserveDecimal(B){
+  return parseFloat(`${B}`)
+}
