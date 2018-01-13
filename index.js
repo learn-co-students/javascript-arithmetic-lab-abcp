eb3d436ee8ef992d56d8dbb71802e6bbd5cb2c10
Crystal <crystal_wright@yahoo.com>
2018-01-13 20:48:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function add (num1, num2) { 
+  var a = num1;
+  var b = num2;
+  return a + b;
+}
+
+function subtract(num1, num2) { 
+  var a = num1;
+  var b = num2;
+  return a - b;
+}
+
+function multiply (num1, num2) { 
+  var a = num1;
+  var b = num2;
+  return a * b;
+}
+
+function divide (num1, num2) { 
+  var a = num1;
+  var b = num2;
+  return a/b;
+}
+
+function inc(num1) { 
+  return ++num1;
+}
+function dec(num1) { 
+  return --num1;
+}
+
+function makeInt(num1){
+  return parseInt(num1,10);
+}
+
+function preserveDecimal(num1){
+  return parseFloat(num1);
+}
+
