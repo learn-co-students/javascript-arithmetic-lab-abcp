ba7f1a89e2d058ef8798420015b39f1e131b1a21
Amanda Bromberg <7b4571c5@opayq.com>
2018-01-14 06:21:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,54 @@
+//ONE
+function add(a,b)
+{
+  return a+b
+}
+
+//TWO
+function subtract(a,b)
+{
+  return a-b
+}
+
+//THREE
+function multiply(a,b)
+{
+  return a*b
+}
+
+//FOUR
+function divide(a,b)
+{
+  return a/b
+}
+
+//FIVE
+function inc(n)
+{
+  n++;
+  return n
+}
+
+//SIX
+function dec(n)
+{
+  n--;
+  return n
+}
+
+//SEVEN
+function makeInt(n)
+{
+  return parseInt(n, 10);
+}
+
+//EIGHT
+function preserveDecimal(n)
+{
+  return parseFloat(n)
+}
+
+
+
+
+
