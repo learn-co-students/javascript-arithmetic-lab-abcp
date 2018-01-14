834527ada523146f8021f5cf649cc846f32a8833
Lori Kulikowski <soupfairy99@gmail.com>
2018-01-14 06:58:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a,b){return a + b}
+add(1,2)
+function subtract(a,b){return a - b}
+subtract(4,3);
+function multiply(a,b){
+  return a * b}
+  multiply(5,6);
+ function divide(a,b){
+  return a/b}
+  divide(30,6); 
+  
+  function inc(n){
+  return ++n}
+  inc(1);
+  
+  function dec(n){
+  return --n}
+  dec(2);
+  
+  function makeInt(n){
+  return parseInt(n,10)
+  }  
+
+makeInt('2.2')
+  
+  function preserveDecimal(n){
+  return parseFloat(n)
+  }  
+
+preserveDecimal('2.222')
+  
+  
+ 
