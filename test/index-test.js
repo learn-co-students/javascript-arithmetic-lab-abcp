a04808462e63055ebfe53a261093e0c809310663
stanciuionelamelania@gmail.com <stanciuionelamelania@gmail.com>
2018-01-19 10:02:48 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -10,7 +10,7 @@ it('add(a, b) adds two numbers and returns the result', function() {
 })
 
 it('subtract(a, b) subtracts b from a and returns the result', function() {
-  expect(subtract(a, b)).toEqual(a - b)
+  expect(substract(a, b)).toEqual(a - b)
 })
 
 it('multiply(a, b) multiplies two numbers and returns the result', function() {
