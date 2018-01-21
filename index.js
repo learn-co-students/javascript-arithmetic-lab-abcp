f1637a67c9499380318abe82cc46ca1a9325c77c
Charlotte Payne <cltpayne@gmail.com>
2018-01-21 14:08:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(a) {
+  return a + 1
+}
+
+function dec(a) {
+  return a - 1
+}
+
+function makeInt(n) {
+  var base = 10
+  var parsed = parseInt(n, base)
+  parseInt("n", base)
+  return parsed 
+}
+
+function preserveDecimal(n) {
+  var parsed = parseFloat(n)
+  parseFloat(n)
+  return parsed
+}
