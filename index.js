32d0a9d40c535541e02de13d9b9bd283449a340c
Megan Poosangtongchai <mpoosangtongchai@gmail.com>
2018-01-24 03:29:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function inc(a) {
+  return inc(a) = (a+1);
+}
