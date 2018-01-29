94055a1fa462232ec56acf22976adf98041d9d17
Novera Lim <lim.novera88@gmail.com>
2018-01-29 19:02:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+var number = (a,b);
+console.log ('8','5');
+parseInt ('8','5');
+function add(a,b) {
+  return (a + b);
+}
+ 
+function subtract(a,b) {
+  return (a - b);
+}
+
+function multiply(a,b) {
+  return (a * b);
+}
+
+function divide(a,b) {
+  return (a / b)
+}
+
+var number = 10;
+function add5() {
+  console.log(number);
+  number += 5;
+}
+function subtract3() {
+  console.log(number)
+  number -= 3;
+}
