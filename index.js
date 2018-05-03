2fb2e9b91c498abe506bc7b661cb93405bfb012c
Smrita Bhetwal <bhetwalsmrita7@gmail.com>
2018-05-03 17:18:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function makeInt(n){
+  var num = parseInt(n, 10)
+  return num
+}
+  
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
+
+function add(a, b){
+  return a + b
+}
+
+function subtract(a, b){
+  return a - b
+}
+
+function multiply(a, b){
+  return a * b
+}
+
+function divide(a, b){
+  return a / b
+}
+
+function inc(n){
+  n++
+  return n
+}
+
+function dec(n){
+  n--
+  return n
+}
